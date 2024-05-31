0d006de4a0905c25602d8941f2a53bd31dcaf15c
sulthanularief148 <sulthanularief148@gmail.com>
2024-05-31 08:56:32 +0530

    Added react and react-dom npm

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
 const header = React.createElement("div", { id: "parent" }, [
   React.createElement("h1", { id: "heading" }, "This is my First React page"),
   React.createElement("h2", { id: "second_head" }, "This is subhead"),
@@ -5,4 +8,4 @@ const header = React.createElement("div", { id: "parent" }, [
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(header);
-console.log(header);
+
