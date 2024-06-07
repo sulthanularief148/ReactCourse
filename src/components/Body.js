3d07f998efc8fa5937eb0b21feb512a93d17b6c3
sulthanularief148 <sulthanularief148@gmail.com>
2024-06-07 07:46:42 +0530

    useState added

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,7 +1,11 @@
 import { RestaurentCard } from "./RestaurentCard";
 import { resList } from "../utils/mockdata";
+import { useState } from "react";
 
 export const Body = () => {
+  //useState it's comes form react, It's normal JS Utility Function.
+  const [listOfRestaurant, setListOfRestaurant] = useState([]);
+
   return (
     <div className="body">
       <div className="filter-btn">
@@ -9,14 +13,18 @@ export const Body = () => {
           style={{ margin: "5px" }}
           className="btn"
           onClick={() => {
-            console.log("Clicked");
+            // const filteredRestaurant = resList.filter(
+            //   (res) => res.info.avgRatingString > 4
+            // );
+            setListOfRestaurant(resList);
           }}
         >
           Filter food
         </button>
       </div>
+
       <div className="res-container">
-        {resList?.map((res) => (
+        {listOfRestaurant?.map((res) => (
           <RestaurentCard resData={res} key={res.info.id} />
         ))}
       </div>
